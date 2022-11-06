3c5c4ede2276313e4b282b11d8e491db8c756e5f
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-11-05 22:39:43 -0800

    return future from lib instead of running it

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! lib.rs
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, dev::Server};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -11,14 +11,14 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .run();
+    Ok(server)
 }
