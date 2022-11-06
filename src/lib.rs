eab1d55ec8dd70c8767a8fd70c7dcb9d2c00fdbe
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-11-05 22:56:03 -0800

    Use random port for testing using TcpListener

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! lib.rs
 
+use std::net::TcpListener;
+
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, dev::Server};
 
 async fn greet(req: HttpRequest) -> impl Responder {
@@ -11,14 +13,14 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run();
+        })
+        .listen(listener)?
+        .run();
     Ok(server)
 }
