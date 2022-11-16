8ec63a59efbfb672e469826aacb6a975feb33b42
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-11-15 22:01:58 -0800

    Add dummy subscriptions end point

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,30 @@
 
 use std::net::TcpListener;
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, dev::Server};
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
-        })
-        .listen(listener)?
-        .run();
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
